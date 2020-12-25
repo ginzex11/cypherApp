8674cc602d41650265d8b526a0d6f180c1c9be56
Ginzex <aginzex@gmail.com>
2020-12-25 03:46:41 +0200

    laying the foundation. for now methods decrypt/encrypt string are up and encrypt file is up main still empty

---
+++ src/CypherFunctions.java
@@ -0,0 +1,57 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class CypherFunctions {
+    Scanner scan = new Scanner(System.in);
+    File file = new File(scan.nextLine() + ".text");
+    private int key;
+
+
+    public int randomKey() {
+        Random random = new Random();
+        return key;
+    }
+
+    public String encryptString(String tmp) throws IOException {
+        char keys = (char) key;
+        String finalTemp = "";
+        for (int i = 0; i < tmp.length(); i++) {
+            char c = (char) (tmp.charAt(i) + keys);
+            finalTemp = new StringBuilder().append(c).toString();
+        }
+        return finalTemp;
+    }
+
+    public String decryptString(String tmp) throws IOException {
+        char keys = (char) key;
+        String finalTemp = "";
+        for (int i = 0; i < tmp.length(); i++) {
+            char c = (char) (tmp.charAt(i) - keys);
+            finalTemp = new StringBuilder().append(c).toString();
+        }
+        return finalTemp;
+    }
+
+    public FileWriter encryptFile(File file) throws IOException {
+        FileWriter newFile = new FileWriter(file.getName() + "_encrypted.txt");
+        try {
+            Scanner readFile = new Scanner(file);
+            while (readFile.hasNextLine()) {
+                String data = encryptString(readFile.nextLine());
+                newFile.write(data);
+            }
+            newFile.close();
+            readFile.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("file empty");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return newFile;
+    }
+
+}
---
+++ src/CypherProgram.java
@@ -0,0 +1,17 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class CypherProgram extends CypherFunctions {
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
